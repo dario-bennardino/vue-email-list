892c5b6c155cd4240b6b1f206906e9d966cf7644
dario.bennardino@gmail.com <dario.bennardino@gmail.com>
2024-03-19 17:27:28 +0100

    [add] inizio bonus

--- Js/script.js
+++ Js/script.js
@@ -8,25 +8,38 @@ createApp({
             apiUrl: 'https://flynn.boolean.careers/exercises/api/random/mail?qty=10',
 
             // CREO UN ARRAY VUOTO DA RIEMPIRE
-            emails: []
+            emails: [],
+            
+            //CONTATORE
+             numResults:0
             
         }
     },
 
     methods:{
         getApi(){
+        
+
             axios.get(this.apiUrl)
             .then((response) => {
                 //se la chiamata va a buon fine 
                 console.log(response.data);
-            this.emails.push(response.data.response);    
+                this.emails.push(response.data.response); 
+                // this.numResults++  
+
 
             })
 
             .catch(errore => {
                 //se la chiamata non va a buon fine
             })
-        }
+        },
+
+        // checkResults(){
+        //     if(this.numResults === 10){
+
+        //     }
+        // }
 
     },
 
