5ead9f43875c49bbd50121129d96020ef26f0613
dario.bennardino@gmail.com <dario.bennardino@gmail.com>
2024-03-19 15:03:10 +0100

    [add] inserito methods getApi

--- Js/script.js
+++ Js/script.js
@@ -3,16 +3,27 @@ const {createApp} = Vue;
 createApp({
     data(){
         return{
-
+            title: 'Lista email'
+            
         }
     },
 
     methods:{
+        getApi(){
+            axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+            .then((risposta) => {
+                //se la chiamata va a buon fine 
+            })
+
+            .catch(errore => {
+                //se la chiamata non va a buon fine
+            })
+        }
 
     },
 
     mounted(){
-        
+        this.getApi();
     }
 
     
